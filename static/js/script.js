26d5a3f85a27030caabdf6fa00e0a28f8f48f324
JohnGC1510 <johngc1510@gmail.com>
2021-04-03 19:54:51 +0000

    commented all functions in script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,17 @@
 
+/*
+Defensive programming function that asks users to confirm in a modal
+before deleting a question
+*/
+
 $(".delete-button").on("click", function(){
    let  questionId = $(this).data("question");
     $("#delete-confirm").attr("href", `/delete_question/${questionId}`);
 });
 
+/*
+Function taken from https://getbootstrap.com/docs/4.0/components/forms/#tooltips to allow for form validation after form has been completed
+*/
 (function() {
   'use strict';
   window.addEventListener('load', function() {
