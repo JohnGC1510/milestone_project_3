9ff2b9f80aae606dee4229cfe7eb9c3384288b1f
JohnGC1510 <johngc1510@gmail.com>
2021-04-03 19:23:39 +0000

    This version has updated sign up form to validate after user input

--- static/js/script.js
+++ static/js/script.js
@@ -4,3 +4,20 @@ $(".delete-button").on("click", function(){
     $("#delete-confirm").attr("href", `/delete_question/${questionId}`);
 });
 
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
+    });
+  }, false);
+})();
